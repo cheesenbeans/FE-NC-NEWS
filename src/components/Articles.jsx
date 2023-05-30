d6ab21829f7808fbd15869cc3ba1f8db492a9405
cheesenbeans <chrishorsley1@hotmail.co.uk>
2023-05-30 14:38:38 +0100

    this commit adds loading functionality to the articles page

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,19 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchArticles } from "../utils/utils";
 import ArticleCard from "./ArticleCard"
 import "../App.css";
 
 function Articles({ articlesList, setArticlesList }) {
-	
-    
+	const [isLoading, setIsLoading] = useState("true")
+
     useEffect(() => {
 		fetchArticles().then((articles) => {
 			setArticlesList(articles);
+			setIsLoading(false)
 		});
 	}, []);
 
     return (
 		<main>
+			{isLoading && <p>Articles are loading...</p>}
 			<h2 className="articleHead">Latest articles...</h2>
 				{articlesList.map((article) => {
 					return <ArticleCard key={article.article_id} article={article} />;
