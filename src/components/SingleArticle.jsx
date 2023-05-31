c02f0fefde73a5b84da55a9fe7c3428d765a54e9
cheesenbeans <chrishorsley1@hotmail.co.uk>
2023-05-31 11:15:51 +0100

    adding commentsLoading functionality and a No Comments message if there are no comments to show

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -5,45 +5,47 @@ import CommentCard from "./CommentCard";
 import "../App.css";
 
 export default function SingleArticle() {
-    const [singleArticle,  setSingleArticle] = useState({})
-    const [commentsByArticleId, setCommentsByArticleId] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-    const {article_id}=useParams()
-    
-    useEffect(()=>{
-        fetchArticleById(article_id).then((article)=>{
-            setSingleArticle(article)
-            setIsLoading(false)
-            return article
-        }).then((article)=> {
-            fetchCommentsByArticleId(article.article_id).then((comments)=>{
-                setCommentsByArticleId(comments)
-            })
-        })
-    }, [])
+  const [singleArticle, setSingleArticle] = useState({});
+  const [commentsByArticleId, setCommentsByArticleId] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [commentsLoading, setCommentsLoading] = useState(true);
+  const { article_id } = useParams();
 
-    return (
-        <article>
-            <div className="article">
-            {(isLoading && <p>This article is loading...</p>)}
-            <h3>{singleArticle.title}</h3>
-            <h4>Written by: {singleArticle.author}</h4>
-            <h4>Topic: {singleArticle.topic}</h4>
-            <p>Votes: {singleArticle.votes}</p>
-            <p>{singleArticle.body}</p>
-            </div>
-            <section className="comments">
-                <h3 className="commentsh3">Comments</h3>
-                    <ul>
-                    {commentsByArticleId.map((comment) => {
-                            return <CommentCard key={comment.comment_id} comment={comment} />;
-                        })
-                    }
-                    </ul>
-            </section>
-        </article>
+  useEffect(() => {
+    fetchArticleById(article_id)
+      .then((article) => {
+        setSingleArticle(article);
+        setIsLoading(false);
+        return article;
+      })
+      .then((article) => {
+        fetchCommentsByArticleId(article.article_id).then((comments) => {
+          setCommentsByArticleId(comments);
+          setCommentsLoading(false);
+        });
+      });
+  }, []);
 
-    )
-
-
+  return (
+    <article>
+      <div className="article">
+        {isLoading && <p>This article is loading...</p>}
+        <h3>{singleArticle.title}</h3>
+        <h4>Written by: {singleArticle.author}</h4>
+        <h4>Topic: {singleArticle.topic}</h4>
+        <p>Votes: {singleArticle.votes}</p>
+        <p>{singleArticle.body}</p>
+      </div>
+      <section className="comments">
+        <h3 className="commentsh3">Comments</h3>
+        {commentsLoading && <p>Comments are loading...</p>}
+        <ul>
+          {(commentsByArticleId.length === 0 && <p>No comments</p> ||
+            commentsByArticleId.map((comment) => {
+              return <CommentCard key={comment.comment_id} comment={comment} />;
+            }))}
+        </ul>
+      </section>
+    </article>
+  );
 }
